2905252ca07005664610652bbf92cce8826cff54
prettyinpurple2021 <prettyinpurple2021@gmail.com>
2025-11-30 02:58:51 -0500

    Fix ES module import in adminUserController: add .js extension

--- backend/src/controllers/adminUserController.ts
+++ backend/src/controllers/adminUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { adminUserService } from '../services/adminUserService';
+import { adminUserService } from '../services/adminUserService.js';
 
 export const adminUserController = {
   /**
