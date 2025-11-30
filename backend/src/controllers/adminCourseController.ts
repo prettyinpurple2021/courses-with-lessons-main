ea235c4e86033ab8a9a2c6d1c6f0be23c48e4fe3
prettyinpurple2021 <prettyinpurple2021@gmail.com>
2025-11-30 02:34:02 -0500

    Fix ES module import in adminCourseController: add .js extension to adminCourseService

--- backend/src/controllers/adminCourseController.ts
+++ backend/src/controllers/adminCourseController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { adminCourseService } from '../services/adminCourseService';
+import { adminCourseService } from '../services/adminCourseService.js';
 
 export const adminCourseController = {
   /**
